f527d14240cc459377bc40ab9b929ab4bd58b7f5
amoshyc <amoshuangyc@gmail.com>
2018-01-13 16:44:52 +0800

    Add preprocess

---
+++ final/gen_cdict.py
@@ -0,0 +1,61 @@
+import re
+import pickle
+from collections import defaultdict
+import pandas as pd
+from tqdm import tqdm
+
+symbol_regex = re.compile(r'[，；．˙]', re.UNICODE)
+change_regex = re.compile(r'[\(（][變又語讀](.*)', re.UNICODE)
+bracket_regex = re.compile(r'[\(（](.*)[\)）]', re.UNICODE)
+
+def process_name(name):
+    if '.gif' in name:
+        return None
+    name = name.replace(' ', '')
+    name = re.sub(symbol_regex, '', name)
+    name = re.sub(bracket_regex, '', name)
+    return name
+
+def process_pron(pron):
+    pron = pron.replace('　', ' ')
+    pron = re.sub(change_regex, '', pron)
+    pron = re.sub(bracket_regex, '', pron)
+    res = []
+    for token in pron.split(' '):
+        if len(token) == 0 or token == '　':
+            continue
+        if len(token) > 1 and token[-1] == 'ㄦ' and token[-2] != ' ':
+            res.append(token[:-1])
+            res.append('ㄦ')
+        else:
+            res.append(token)
+    return res
+
+df1 = pd.read_excel('data/cdict1.xls')[['字詞名', '注音一式']]
+df2 = pd.read_excel('data/cdict2.xls')[['字詞名', '注音一式']]
+df3 = pd.read_excel('data/cdict3.xls')[['字詞名', '注音一式']]
+df = pd.concat([df1, df2, df3])
+df = df.dropna(axis=0)
+
+with tqdm(ascii=True, total=len(df)) as pbar:
+    d = dict()
+    for idx, row in df.iterrows():
+        name, pron = row['字詞名'], row['注音一式']
+        new_name = process_name(name)
+        new_pron = process_pron(pron)
+
+        if not new_name or not new_pron:
+            continue
+
+        if len(new_name) != len(new_pron):
+            print('|{}| => |{}|'.format(name, pron))
+            print('|{}| => {}'.format(new_name, new_pron))
+            assert False
+
+        if new_name not in d:
+            d[new_name] = new_pron
+        
+        pbar.update()
+
+with open('data/dict.pkl', 'wb') as f:
+    pickle.dump(d, f)
---
+++ final/gen_table.py
@@ -0,0 +1,57 @@
+import pickle
+from collections import defaultdict
+
+import pandas as pd
+from tqdm import tqdm
+
+with open('data/dict.pkl', 'rb') as f:
+    pron = pickle.load(f)
+
+def get_first_syllable(pron):
+    if pron[0] == '˙':
+        return pron[1] + '0'
+    else:
+        idx = 'ˊˇˋ'.find(pron[-1])
+        if idx == -1:
+            return pron[0] + '1'
+        return pron[0] + str(idx + 2)
+
+def get_keys(word):
+    if word in pron:
+        keys = [get_first_syllable(char) for char in pron[word]]
+    else:
+        if any((char not in pron) for char in word):
+            return None
+        keys = [get_first_syllable(pron[char]) for char in word]
+    return ''.join(keys)
+
+
+df = pd.read_csv('data/essay.txt', sep='\t', header=None, names=['word', 'freq'])
+
+essay_words = set(df['word'])
+cdict_words = set(pron.keys())
+words = essay_words | cdict_words
+
+with tqdm(total=len(words)) as pbar:
+    table = defaultdict(list) # 碼表
+    for word in words:
+        keys = get_keys(word)
+        if keys:
+            table[keys].append(word)
+        pbar.update()
+
+with tqdm(total=len(words)) as pbar:
+    freq = {} # 詞頻表
+    for _, r in df.iterrows():
+        freq[r['word']] = int(r['freq'])
+        pbar.update()
+    for word in cdict_words:
+        if word not in freq:
+            freq[word] = 0
+        pbar.update()
+
+with open('data/table.pkl', 'wb') as f:
+    pickle.dump(table, f)
+
+with open('data/freq.pkl', 'wb') as f:
+    pickle.dump(freq, f)
